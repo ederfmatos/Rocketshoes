5baa20815b505b210464a4ea00847c92ffef7a26
ederfmatos <ederfmatos@gmail.com>
2020-04-05 19:57:14 -0300

    Estilizaçao do Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import {
+	MdRemoveCircleOutline,
+	MdAddCircleOutline,
+	MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-	return <div />;
+	return (
+		<Container>
+			<ProductTable>
+				<thead>
+					<tr>
+						<th />
+						<th>Produto</th>
+						<th>Quantidade</th>
+						<th>Subtotal</th>
+						<th />
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>
+							<img
+								src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/70/HZM-1731-270/HZM-1731-270_zoom2.jpg?ts=1573579328&ims=326x"
+								alt="Tênis"
+							/>
+						</td>
+						<td>
+							<strong>Tenis mui massa</strong>
+							<span>R$129,90</span>
+						</td>
+
+						<td>
+							<div>
+								<button type="button">
+									<MdRemoveCircleOutline size={20} color="#7159c1" />
+								</button>
+
+								<input type="number" readOnly value={1} />
+
+								<button type="button">
+									<MdAddCircleOutline size={20} color="#7159c1" />
+								</button>
+							</div>
+						</td>
+
+						<td>
+							<strong>R$ 258,80</strong>
+						</td>
+
+						<td>
+							<button type="button">
+								<MdDelete size={20} color="#7159c1" />
+							</button>
+						</td>
+					</tr>
+				</tbody>
+			</ProductTable>
+
+			<footer>
+				<button type="button">Finalizar Pedido</button>
+
+				<Total>
+					<span>Total</span>
+					<strong>R$ 1920,28</strong>
+				</Total>
+			</footer>
+		</Container>
+	);
 }
